171fc57c4404703a6f5a69c95cec744d3e4ab76a
wlsql852 <wlsql852@naver.com>
2024-08-22 15:35:52 +0900

    같은 타입의 Bean이 두개라면

---
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -0,0 +1,26 @@
+package com.sparta.springauth;
+
+import com.sparta.springauth.food.Food;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+public class BeanTest {
+
+    @Autowired
+    Food pizza;   //food로 하면 등록된 Bean이 두개이기 때문에 bean의 이름을 지정해줌
+
+    @Autowired
+    Food chicken;
+
+    @Test
+    @DisplayName("테스트")
+    void test1 () {
+        pizza.eat();
+        chicken.eat();
+    }
+}
